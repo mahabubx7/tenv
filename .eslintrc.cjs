bd3a5694d49bef1e8172e7b737450e8f7534e88b
Mahabub <mahabubx7@gmail.com>
2024-01-29 21:46:34 +0600

    :bug: fix(config): jest-runner config fixes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,5 +3,6 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  ignorePatters: ['./jest.config.js', './commitlint.config.js', './lib'],
   root: true,
 };
