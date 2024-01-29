43cb0f7146758b65bdcbaa88aa017a5c975ab95c
Mahabub <mahabubx7@gmail.com>
2024-01-30 02:48:17 +0600

    :test_tube: test(module): env specs

--- src/Env.spec.ts
+++ src/Env.spec.ts
@@ -1,4 +1,7 @@
 import Env from './Env';
+import 'dotenv/config';
+
+const env = new Env({ ...(process.env as Record<string, string>) });
 
 /**
  * @jest-environment node
@@ -9,9 +12,59 @@ describe('class: Env test suite (unit)', () => {
     expect(typeof process.env.NODE_ENV).toBe('string' ?? 'undefined');
   });
 
-  it('should return env:PORT value or undefined', () => {
-    const port = Env.get<number>('PORT');
-    if (port) expect(typeof port).toBe('number');
-    else expect(port).toBeUndefined();
+  it('should return env:NAME value or undefined', () => {
+    const name = env.key<string>('NAME').get();
+    expect(typeof name).toBe('undefined');
+    console.log('env:NAME ', name);
+    expect(name).toBeUndefined();
+  });
+
+  it('should return env:PORT value as required!', () => {
+    const port = env.key<number>('PORT', true).integer().get();
+    expect(port).toBeTruthy();
+    expect(typeof port).toBe('number');
+  });
+
+  it('should return env:ALLOW value as boolean', () => {
+    const allow = env.key<boolean>('ALLOW', true).bool().get();
+    expect(typeof allow).toBe('boolean');
+  });
+
+  it('should return env:EMAIL value as email-address', () => {
+    const email = env.key<string>('EMAIL', true).email().get();
+    expect(typeof email).toBe('string');
+  });
+
+  it('should throw error for env:EMAIL2 as invalid email-address', () => {
+    expect(() => env.key<string>('EMAIL2', true).email().get()).toThrow(
+      'Invalid email address!',
+    );
+  });
+
+  it('should return env:WEB value as url', () => {
+    const url = env.key<string>('WEB', true).url().get();
+    expect(typeof url).toBe('string');
+  });
+
+  it('should return env:HOST value as url', () => {
+    const url = env.key<string>('HOST', true).url().get();
+    expect(typeof url).toBe('string');
+  });
+
+  it('should return env:IPV6 value as url', () => {
+    const url = env.key<string>('IPV6', true).url({ ipv6: true }).get();
+    expect(typeof url).toBe('string');
+  });
+
+  it('should return env:LOCAL value as url', () => {
+    const url = env.key<string>('LOCAL', true).url().get();
+    expect(typeof url).toBe('string');
+  });
+
+  it('should return if it is in Test mode?', () => {
+    expect(env.isProduction).toBeFalsy();
+    expect(env.isDevelopment).toBeFalsy();
+    expect(env.isStaging).toBeFalsy();
+    expect(env.isTest).toBeTruthy();
   });
 });
