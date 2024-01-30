c52c0edb8c324834920e9c218e0d9cb14a019736
Mahabub <mahabubx7@gmail.com>
2024-01-31 00:35:25 +0600

    :recycle: refactor(config): legacy supports with nodejs and more

--- tsup.config.ts
+++ tsup.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
   minify: true,
   outDir: 'lib',
   tsconfig: 'tsconfig.build.json',
-  target: ['es5', 'es6'],
+  target: ['es5', 'es6', 'es2022', 'esnext', 'node16', 'node18', 'node20'],
 });
